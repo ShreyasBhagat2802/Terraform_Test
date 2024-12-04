15f3883e416c36355322588bbaed828dd1cea2bc
shreyas <shreyasbhagat2802@gmail.com>
2024-12-04 12:33:32 +0000

    second

---
+++ dev.tfvars
@@ -0,0 +1,2 @@
+ami           = "ami-02f5bda515af4b65e"
+instance_type = "t2.small"
---
+++ variable.tf
@@ -0,0 +1,7 @@
+variable "ami" {
+  description = "ami for instacnce"
+}
+
+variable "instance_type" {
+  description = "Instance Type"
+}
